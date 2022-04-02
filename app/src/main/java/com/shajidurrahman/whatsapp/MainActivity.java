8d5143d0e03df491621e374b8ef260dcc7fe0a41
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-04-02 21:34:58 +0600

    first commit

--- app/src/main/java/com/shajidurrahman/whatsapp/MainActivity.java
+++ app/src/main/java/com/shajidurrahman/whatsapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.shajidurrahman.whatsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shajidurrahman.whatsapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
---
+++ app/src/main/java/com/shajidurrahman/whatsapp/SIgnUpActivity.java
@@ -0,0 +1,14 @@
+package com.shajidurrahman.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SIgnUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
